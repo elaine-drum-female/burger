ee7668fa4f8d86cfd48706899af108456d2677a7
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-03-22 21:41:39 -0500

    Added seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', TRUE);
